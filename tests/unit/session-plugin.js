17ce2a87290c21f11309df8e22910cc693522b95
Hysios Hu <hysios@gmail.com>
2014-11-22 23:46:40 +0800

    first commit

---
+++ tests/unit/session-plugin.js
@@ -0,0 +1,98 @@
+var sessionPlugin = require('../../lib/session-plugin');
+var FluxibleApp = require('fluxible-app');
+var expect = require('chai').expect;
+
+describe('sessionPlugin', function(){
+	var app,
+		sessionMock,
+		pluginInstance,
+		context;
+
+	beforeEach(function() {
+		sessionMock = {
+			user: {
+				name: 'hysios',
+				email: 'hysios@gmail.com'
+			}
+		};
+		app = new FluxibleApp();
+		pluginInstance = sessionPlugin();
+		app.plug(pluginInstance);
+
+		context = app.createContext({
+			session: sessionMock
+		});
+	});
+
+	describe('actionContext', function() {
+		var actionContext;
+		beforeEach(function() {
+			actionContext = context.getActionContext();
+		});
+
+		it('getSession', function() {
+			expect(actionContext.getSession).to.be.an('function');
+		});
+
+		it('setSesion', function() {
+			expect(actionContext.setSession).to.be.an('function');
+		});
+	});
+
+	describe('componentContext', function() {
+		var componentContext;
+		beforeEach(function() {
+			componentContext = context.getComponentContext();
+		});
+
+		it('getSession', function() {
+			expect(componentContext.getSession).to.be.an('function');
+		});
+
+		it('setSesion', function() {
+			expect(componentContext.setSession).to.be.an('function');
+		});
+	});
+
+	describe('storeContext', function() {
+		var storeContext;
+		beforeEach(function() {
+			storeContext = context.getStoreContext();
+		});
+
+		it('getSession', function() {
+			expect(storeContext.getSession).to.be.an('function');
+		});
+
+		it('setSesion', function() {
+			expect(storeContext.setSession).to.be.an('function');
+		});
+	});
+
+    describe('dehydrate', function () {
+        it('should dehydrate its state correctly', function () {
+            expect(pluginInstance.dehydrate()).to.deep.equal({
+                user: {
+                	name: 'hysios',
+                	email: 'hysios@gmail.com'
+                }
+            });
+        });
+    });
+
+    describe('rehydrate', function () {
+        it('should rehydrate the state correctly', function () {
+            pluginInstance.rehydrate({
+                user: {
+                	name: 'bob'
+                }
+            });
+            expect(pluginInstance.dehydrate()).to.deep.equal({
+                user: {
+                	name: 'bob'
+                }
+            });
+        });
+    });
+
+});
